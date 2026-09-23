bd8c4343bbb91e82de3ebb1711142e07d3036d0b
Alex <alex@atoms.dev>
2026-09-23 16:50:37 -0700

    style: шрифт «Практики чтения» как в «Информации о слове»

--- js/stats.js
+++ js/stats.js
@@ -118,7 +118,7 @@ function updateSwapBtnTitle() {
 }
 
 /* ----- Service worker ----- */
-const SERVICE_WORKER_URL = "sw.js?v=3.20.69";
+const SERVICE_WORKER_URL = "sw.js?v=3.20.70";
 let swRegistration = null;
 let swRefreshing = false;
 let swUpdateApplying = false;
